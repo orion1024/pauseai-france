307f3cd4aa374629dad6086d78bfa575354546c8
Mathieu Bourrier <mathieu.bourrier@easyteam.fr>
2025-04-02 13:21:16 +0200

    TMP preparing for initial PR

--- svelte.config.js
+++ svelte.config.js
@@ -4,20 +4,11 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
 import { mdsvex, escapeSvelte } from 'mdsvex'
 import { getHighlighter } from 'shiki'
 import remarkUnwrapImages from 'remark-unwrap-images'
-
-// TODO clean comments
-// import remarkParse from 'remark-parse'
-// import remarkRehype from 'remark-rehype'
-// import rehypeStringify from 'rehype-stringify'
-// import remarkDirective from 'remark-directive'
-// import {rehypeCustomClass} from './src/lib/rehypeCustomclass.js'
-
 import remarkToc from 'remark-toc'
 import rehypeSlug from 'rehype-slug'
 import { faqPlugin } from './src/lib/faqPlugin.js'
 import { remarkFrenchTypography } from './src/lib/typographyPlugin.js'
-import { rehypeWBWPopups2 } from './src/lib/rehypeWBWPlugins.js'
-import { rehypeWBWBlueBoxes } from './src/lib/rehypeWBWPlugins.js'
+import { rehypeWBWBlueBoxes, rehypeWBWPopups2 } from './src/lib/rehypeWBWPlugins.js'
 
 import { config as dotenv } from 'dotenv'
 dotenv()
@@ -36,23 +27,14 @@ const mdsvexOptions = {
 			return `{@html \`${html}\` }`
 		}
 	},
-	remarkPlugins: [
-		// remarkParse,
-		// remarkDirective,
-		// rehypeWBWBlueBoxes,
-		// rehypeWBWPopups,
-		// [remarkRehype, {allowDangerousHtml: true}],
-		// [rehypeStringify, {allowDangerousHtml: true}],
-		remarkUnwrapImages,
-		[remarkToc, { tight: true }],
-		remarkFrenchTypography
-	],
+	remarkPlugins: [remarkUnwrapImages, [remarkToc, { tight: true }], remarkFrenchTypography],
 	rehypePlugins: [rehypeWBWBlueBoxes, rehypeWBWPopups2, rehypeSlug, faqPlugin]
 }
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	extensions: ['.svelte', '.md'],
-	preprocess: [mdsvex(mdsvexOptions), vitePreprocess()],
+	preprocess: [vitePreprocess(), mdsvex(mdsvexOptions)],
 	kit: {
 		adapter: adapter({
 			edge: true
